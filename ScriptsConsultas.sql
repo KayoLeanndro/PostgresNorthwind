8260073ccdc4fcae07d4e6a860aec28e2ba93dc8
Kayo Leanndro <100872126+KayoLeanndro@users.noreply.github.com>
2025-11-21 16:51:44 -0300

    Update ScriptsConsultas.sql

--- ScriptsConsultas.sql
+++ ScriptsConsultas.sql
@@ -42,10 +42,26 @@ from orders
 order by order_id asc;
 
 -----------------------------------------------------------
-
+--Utiliza case when com o extract year from age para buscar os funcionarios que tem mais de 5 anos de empresa
 select employee_id, concat(e.first_name, ' ', e.last_name), e.hire_date,
 case when extract(year from age('1998-01-01'::date, hire_date)) >= 5 then 'Senior'
 else 'Junior'end as level
 from employees e;
+
+-----------------------------------------------------------
+--Utiliza o Limit para limitar a quantidade de registros
+select * from employees e limit 5;
+
+--Utiliza o random para que nao venham os mesmos 5 registros
+select * from employees e order by random() limit 5;
+
+-----------------------------------------------------------
+
+--Utilizando o NULL e o not NULL para verificar campo especifico em branco
+select * from employees e where e.reports_to is not null;
+
+select * from employees e where e.reports_to is null;
+
+-----------------------------------------------------------
 
 
